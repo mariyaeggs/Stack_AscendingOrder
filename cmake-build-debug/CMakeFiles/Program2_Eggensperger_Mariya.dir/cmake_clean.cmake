ce45d404fc5de01d69aff02d60263887951b533f
mariyaeggs <mariyawilson@live.com>
2017-01-16 14:55:29 -0500

    Reimplement previouse Stack.h and Stack.cpp files. Solve tester file.

---
+++ cmake-build-debug/CMakeFiles/Program2_Eggensperger_Mariya.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Program2_Eggensperger_Mariya.dir/Stack_Tester.cpp.o"
+  "Program2_Eggensperger_Mariya.pdb"
+  "Program2_Eggensperger_Mariya"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Program2_Eggensperger_Mariya.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
