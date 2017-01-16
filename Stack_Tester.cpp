d8cccaaf93d9e7f3dc1e6e84b459d3c1b4e8bf6c
mariyaeggs <mariyawilson@live.com>
2017-01-16 14:54:48 -0500

    Initial commit

---
+++ Stack_Tester.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+#include "Stack.h"
+/**
+ * Title: Lab 2 Submission : Stacks
+ * Course: CST 370 Design & Analysis of Algorithms
+ * School: CSU, Monterey Bay
+ * Professor: Dr. Feiling Jia
+ * Source: CLion 2016.3.2 on 1/15/2017
+ *
+ * Driver file for a stacks program that sorts a
+ * group of numbers in ascending order using two
+ * stacks. Assumed, the numbers are initially
+ * given in one of the stacks.
+ *
+ * @author Mariya Eggensperger
+*/
+int main() {
+   Stack s1, s2;
+
+   // Test 1, 5 -4, 6, 2
+   s1.push(1);
+   s1.push(5);
+   s1.push(-4);
+   s1.push(6);
+   s1.push(2);
+}
